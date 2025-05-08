37f4d26fd266895a02d52659f76ca44b2554b9d8
Magictea <sherbakovdaniil6@gmail.com>
2025-05-08 12:15:45 +0300

    feat(exceptions): Добавлена обработка исключений, когда сессия истекла

--- teawish/web/exceptions.py
+++ teawish/web/exceptions.py
@@ -1,10 +1,12 @@
 from functools import partial
+from tkinter.font import names
 
 from fastapi import FastAPI
 from fastapi.requests import Request
 from fastapi.templating import Jinja2Templates
 from fastapi.responses import HTMLResponse
 
+from teawish.application.auth.exceptions import ExpiredSessionException
 from teawish.application.user.exceptions import UserDoesNotExistsException
 from teawish.web.utils import is_htmx_request
 
@@ -37,3 +39,5 @@ def setup_exception_handlers(app: FastAPI, templates: Jinja2Templates):
     app.add_exception_handler(500, partial(exception_500_template_handler, templates=templates))
     app.add_exception_handler(404, partial(exception_404_template_handler, templates=templates))
     app.add_exception_handler(UserDoesNotExistsException, partial(exception_user_not_found_template_handler, templates=templates))
+    app.add_exception_handler(ExpiredSessionException, partial(exception_user_not_found_template_handler, templates=templates))
+
