0982d719bebc8f005564cb6a971a35740adc1d10
Magictea <sherbakovdaniil6@gmail.com>
2025-05-09 16:08:44 +0300

    feat(use_cases): админская бл вынесена в интеракторы, мелкие исправления

---
+++ teawish/application/admin/use_cases/admin.py
@@ -0,0 +1,29 @@
+import dataclasses as dc
+import logging
+from uuid import UUID
+
+from teawish.application.admin.exceptions import AccessDenied
+from teawish.application.auth.interfaces import (
+    ISessionRepository,
+    SessionStorageFilter,
+)
+from teawish.application.user.models import User
+
+log = logging.getLogger(__name__)
+
+
+class GetAdminPageUseCase:
+    def __init__(
+            self,
+            session_repository: ISessionRepository,
+    ):
+        self._session_repository = session_repository
+
+    async def __call__(self, session_id: UUID) -> User:
+        user: User = await self._session_repository.get_user(SessionStorageFilter(session_id=session_id))
+
+        if not user.is_admin:
+            log.warning(f'Trying to open admin page without permissions: {dc.asdict(user)}')
+            raise AccessDenied
+
+        return user
---
+++ teawish/application/admin/use_cases/user.py
@@ -0,0 +1,38 @@
+import dataclasses as dc
+import logging
+from uuid import UUID
+
+from teawish.application.admin.exceptions import AccessDenied
+from teawish.application.auth.interfaces import (
+    ISessionRepository,
+    SessionStorageFilter,
+)
+from teawish.application.user.interfaces import IUserRepository
+from teawish.application.user.models import User
+
+log = logging.getLogger(__name__)
+
+
+class GetUsersListPageUseCase:
+    def __init__(
+            self,
+            user_repository: IUserRepository,
+            session_repository: ISessionRepository,
+    ):
+        self._user_repository = user_repository
+        self._session_repository = session_repository
+
+    async def __call__(self, session_id: UUID, limit: int, offset: int) -> dict:
+        user: User = await self._session_repository.get_user(SessionStorageFilter(session_id=session_id))
+
+        if not user.is_admin:
+            log.warning(f'Trying to open admin page without permissions: {dc.asdict(user)}')
+            raise AccessDenied
+
+        users: list[User] = await self._user_repository.get_list(limit, offset)
+        total_count: int = await self._user_repository.total_count()
+
+        return {
+            'objects': users,
+            'total_count': total_count,
+        }
